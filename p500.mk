9392c62f17507df302f48c9a326e1aaa4dd37ec1
Pasquale Lodise <pakilodi@gmail.com>
2012-04-11 11:31:02 +0200

    use dalvik params for 512mb device

--- p500.mk
+++ p500.mk
@@ -130,6 +130,9 @@ PRODUCT_PACKAGES += \
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 
+#Use dalvik parameters for a 512 MB device
+$(call inherit-product, frameworks/base/build/phone-hdpi-512-dalvik-heap.mk)
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
